f1ff0b73187124c01529fe1dd897d97e7e4dfad1
mxales11 <mxales11@smumn.edu>
2013-02-11 14:38:37 -0600

    changing helper

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -10,7 +10,7 @@ module NewsHelper
 
 		out = "<h4>Requirements table</h4> <span class = 'label label-important'>Requirements</span><table class = 'table-striped table-bordered'>"
 		Requirement.find_each do |requirement|
-			out = out + "<tr><td>#{requirement[:description]}</td><td>#{requirement[:points]}</td></tr>"
+			out = out + "<tr><td>#{requirement[:describtion]}</td><td>#{requirement[:points]}</td></tr>"
 		end
 		out = out + "</table>"
 		out.html_safe
